a16276efe99e6374cb14930013ac584c7708e14a
David Soller <d.r.soller3@gmail.com>
2014-10-04 12:52:08 -0400

    Added Cell interaction methods
    Added name, team, troops, updateCell

--- t12risk/app.js
+++ t12risk/app.js
@@ -1,9 +1,25 @@
 ﻿var Cell = (function () {
     function Cell(name, team, troops) {
-        this.name = name;
-        this.team = team;
-        this.troops = troops;
+        this.nameVal = name;
+        this.teamVal = team;
+        this.troopsVal = troops;
     }
+    Cell.prototype.name = function () {
+        return this.nameVal;
+    };
+
+    Cell.prototype.team = function () {
+        return this.teamVal;
+    };
+
+    Cell.prototype.troops = function () {
+        return this.troopsVal;
+    };
+
+    Cell.prototype.updateCell = function (team, troops) {
+        this.teamVal = team;
+        this.troopsVal = troops;
+    };
     return Cell;
 })();
 
@@ -15,6 +31,10 @@ var Control = (function () {
         var b1 = cs({ name: "b1", team: "blue", troops: 10 });
         var b2 = cs({ name: "b2", team: "blue", troops: 10 });
     }
+    Control.prototype.attack = function (attacker, defender) {
+        //defender.troops = 0;
+        defender.team = attacker.team;
+    };
     return Control;
 })();
 
--- t12risk/app.ts
+++ t12risk/app.ts
@@ -1,32 +1,54 @@
-﻿﻿interface CellStatic {
-     new (name: string, team: string, troops: number);
+﻿interface CellStatic {
+    new (name: string, team: string, troops: number);
 }
 
 class Cell {
-    private name;
-    private team;
-    private troops;
+    private nameVal;
+    private teamVal;
+    private troopsVal;
+
+    name() {
+        return this.nameVal;
+    }
+
+    team() {
+        return this.teamVal;
+    }
+
+    troops() {
+        return this.troopsVal;
+    }
+
+    updateCell(team: string, troops: number) {
+        this.teamVal = team;
+        this.troopsVal = troops;
+    }
 
     constructor(name: string, team: string, troops: number) {
-        this.name = name;
-        this.team = team;
-        this.troops = troops;
+        this.nameVal = name;
+        this.teamVal = team;
+        this.troopsVal = troops;
     }
 }
 
- class Control {
-     a1;
+class Control {
+    a1;
 
-     constructor() {
+    constructor() {
         var cs: CellStatic = Cell;
         this.a1 = cs({ name: "a1", team: "red", troops: 10 });
         var a2 = cs({ name: "a2", team: "red", troops: 10 });
         var b1 = cs({ name: "b1", team: "blue", troops: 10 });
         var b2 = cs({ name: "b2", team: "blue", troops: 10 });
-     }
+    }
+
+    attack(attacker: Cell, defender: Cell) {
+        //defender.troops = 0;
+        defender.team = attacker.team;
+    }
 }
 
- var c = new Control();
- console.log(c.a1);
+var c = new Control();
+console.log(c.a1);
 
 
