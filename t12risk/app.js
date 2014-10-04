375a123f3d57b409ca2ba5b50de615d4aabceba0
David Soller <d.r.soller3@gmail.com>
2014-10-04 05:19:33 -0400

    Now we are lost!
    Lost state for current commit... :confounded:

--- t12risk/app.js
+++ t12risk/app.js
@@ -7,11 +7,14 @@
 var Control = (function () {
     function Control() {
         var cs = Cell;
-        var a1 = cs({ name: "a1", team: "red", troops: 10 });
+        this.a1 = cs({ name: "a1", team: "red", troops: 10 });
         var a2 = cs({ name: "a2", team: "red", troops: 10 });
         var b1 = cs({ name: "b1", team: "blue", troops: 10 });
         var b2 = cs({ name: "b2", team: "blue", troops: 10 });
     }
     return Control;
 })();
+
+var c = new Control();
+console.log(c.a1);
 //# sourceMappingURL=app.js.map
--- t12risk/app.ts
+++ t12risk/app.ts
@@ -1,21 +1,24 @@
 ﻿﻿interface CellStatic {
-    new (name: string, team: string, troops: number);
+     new (name: string, team: string, troops: number);
 }
 
 class Cell {
     constructor(name: string, team: string, troops: number) { }
 }
 
-class Control {
+ class Control {
+     a1;
+
      constructor() {
         var cs: CellStatic = Cell;
-        var a1 = cs({ name: "a1", team: "red", troops: 10 });
+        this.a1 = cs({ name: "a1", team: "red", troops: 10 });
         var a2 = cs({ name: "a2", team: "red", troops: 10 });
         var b1 = cs({ name: "b1", team: "blue", troops: 10 });
         var b2 = cs({ name: "b2", team: "blue", troops: 10 });
      }
 }
 
-
+ var c = new Control();
+ console.log(c.a1);
 
 
